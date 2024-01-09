8d451ddb40bb9743e6068f2617fc5086f812e69e
Dima Berry <dimaberry1@gmail.com>
2024-01-09 20:10:45 +0100

    feat: create a functional render two tracks

---
+++ index.js
@@ -0,0 +1,77 @@
+// const titleElement = document.createElement("h1");
+// titleElement.append("Hello my dear!");
+// document.body.append(titleElement);
+
+// const techSelectElement = document.createElement("select");
+// const techSelectOption0Element = document.createElement("option");
+// techSelectOption0Element.value = 1;
+// techSelectOption0Element.append("JS");
+// techSelectElement.append(techSelectOption0Element);
+
+// const techSelectOption1Element = document.createElement("option");
+// techSelectOption1Element.value = 2;
+// techSelectOption1Element.append("HTML");
+// techSelectElement.append(techSelectOption1Element);
+
+// document.body.append(techSelectElement);
+
+// const title = "incubator";
+// const graduatesCount = 2000;
+// const areYouReady = true;
+
+// let pageTitleElement = document.createElement("h1");
+// pageTitleElement.innerText = title;
+// document.body.append(pageTitleElement);
+
+// let graduatesCountElement = document.createElement("input");
+// graduatesCountElement.value = graduatesCount;
+// document.body.append(graduatesCountElement);
+
+// let areYouReadyElement = document.createElement("input");
+// areYouReadyElement.type = "checkbox";
+// areYouReadyElement.value = areYouReady;
+// areYouReadyElement.checked = "true";
+// document.body.append(areYouReadyElement);
+
+// const date = {
+//   title: "incubator",
+//   graduatesCount: 2000,
+//   areYouReady: true,
+//   technologies: [
+//     { id: 1, title: "Front" },
+//     { id: 2, title: "Back" },
+//     { id: 3, title: "DevOps" },
+//   ],
+// };
+
+// let pageTitleElement = document.createElement("h1");
+// pageTitleElement.innerText = date.title;
+// document.body.append(pageTitleElement);
+
+// let graduatesCountElement = document.createElement("input");
+// graduatesCountElement.value = date.graduatesCount;
+// document.body.append(graduatesCountElement);
+
+// let areYouReadyElement = document.createElement("input");
+// areYouReadyElement.type = "checkbox";
+// areYouReadyElement.value = date.areYouReady;
+// areYouReadyElement.checked = "true";
+// document.body.append(areYouReadyElement);
+
+// let technologiesSelectElement = document.createElement("select");
+// document.body.append(technologiesSelectElement);
+
+// let technologiesOption0Element = document.createElement("option");
+// // technologiesOption0Element.value = date.technologies[0];
+// technologiesOption0Element.innerText = date.technologies[0].title;
+// technologiesSelectElement.append(technologiesOption0Element);
+
+// let technologiesOption1Element = document.createElement("option");
+// // technologiesOption0Element.value = date.technologies[1];
+// technologiesOption1Element.innerText = date.technologies[1].title;
+// technologiesSelectElement.append(technologiesOption1Element);
+
+// let technologiesOption2Element = document.createElement("option");
+// // technologiesOption0Element.value = date.technologies[2];
+// technologiesOption2Element.innerText = date.technologies[2].title;
+// technologiesSelectElement.append(technologiesOption2Element);
